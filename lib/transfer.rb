cf7b1ba1e15ccb0e36f435d0960debe339002f2a
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-08-27 06:37:46 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,18 @@
 class Transfer
   # your code here
+  
+  attr_reader :sender, :receiver, :amount
+  attr_accessor :status
+  
+  def initialize(sender, receiver, status = "pending", amount)
+    @sender = sender
+    @receiver = receiver
+    @status = status
+    @amount = amount
+  end
+  
+  def valid?
+    
+  end 
+  
 end
